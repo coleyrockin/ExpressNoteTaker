6e60e721501914af1c186daea589fca430f7413e
coleyrockin <coleyrockin@aol.com>
2022-08-09 20:40:16 -0500

    create new note

--- server.js
+++ server.js
@@ -1,22 +1,49 @@
-
+//Requires
 const PORT = process.env.PORT || 3001;
 const fs = require('fs');
 const path = require('path');
-
 const express = require('express');
 const app = express();
-
 const allNotes = require('./db/db.json');
 
-// Setup data parsing
+//Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-//Require routes file
-require('./routes/routes')(app);
+//Routes
+app.get('/api/notes', (req, res) => {
+    res.json(allNotes.slice(1));
+});
 
-// Setup listener
-app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
-});  
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+//CREATE NEW NOTE
+function createNewNote(body, notesArray) {
+    const newNote = body;
+    if (!Array.isArray(notesArray))
+        notesArray = [];
+    
+    if (notesArray.length === 0)
+        notesArray.push(0);
+
+    body.id = notesArray[0];
+    notesArray[0]++;
+
+    notesArray.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(notesArray, null, 2)
+    );
+    return newNote;
+}
